e55ba6595a8894017d1088727758af0dc35f4207
Irera Mukawera Jockebed <irerantamushobora@gmail.com>
2026-03-06 16:54:18 +0200

    Added RestaurantTableBookingApplicationTests

---
+++ src/test/java/auca/ac/rw/RestaurantTableBooking/RestaurantTableBookingApplicationTests.java
@@ -0,0 +1,13 @@
+package auca.ac.rw.RestaurantTableBooking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantTableBookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
