cf22b51ac4ac80a32fea6f1d47f39b5febea9f67
Irera Mukawera Jockebed <irerantamushobora@gmail.com>
2026-03-10 10:18:30 +0200

    Added BookingRepository - existsByCustomerIdAndTableId, findByCustomerId

--- src/main/java/auca/ac/rw/RestaurantTableBooking/repository/BookingRepository.java
+++ src/main/java/auca/ac/rw/RestaurantTableBooking/repository/BookingRepository.java
@@ -11,11 +11,9 @@ import auca.ac.rw.RestaurantTableBooking.modal.Booking;
 @Repository
 public interface BookingRepository extends JpaRepository<Booking, UUID> {
 
-    // existsBy → checks if booking already exists
-    // for this customer and table
+    
     Boolean existsByCustomerIdAndTableId(UUID customerId, UUID tableId);
 
-    // Get all bookings for a specific customer
     List<Booking> findByCustomerId(UUID customerId);
 
 }
