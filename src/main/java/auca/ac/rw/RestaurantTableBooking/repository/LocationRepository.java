083754a24e849deb8b6309175a1c187766000c47
Irera Mukawera Jockebed <irerantamushobora@gmail.com>
2026-03-10 10:17:46 +0200

    Added LocationRepository - existsByCode, findByCode, findByName

--- src/main/java/auca/ac/rw/RestaurantTableBooking/repository/LocationRepository.java
+++ src/main/java/auca/ac/rw/RestaurantTableBooking/repository/LocationRepository.java
@@ -8,13 +8,10 @@ import java.util.UUID;
 @Repository
 public interface LocationRepository extends JpaRepository<Location, UUID> {
 
-    // existsBy → checks if location with this code already exists
-    // Requirement 7: existsBy() method
+    
     Boolean existsByCode(String code);
 
-    // find location by code → used for province query
     Location findByCode(String code);
 
-    // find location by name → used for province query
     Location findByName(String name);
 }
